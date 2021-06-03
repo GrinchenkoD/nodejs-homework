d143c39c4bfb6c0bc5df2e5ea9eb1105846b6a08
Dmitry Grinchenko <ra1d3r@i.ua>
2021-06-03 11:11:43 +0300

    auth

---
+++ helpers/constants.js
@@ -0,0 +1,13 @@
+const HttpCode = {
+  OK: 200,
+  CREATED: 201,
+  NO_CONTENT: 204,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  CONFLICT: 409,
+};
+
+module.exports = {
+  HttpCode,
+};
