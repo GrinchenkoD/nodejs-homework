f6192e9177cb60fbaec93750c29bfcdb6cdb200b
Dmitry Grinchenko <ra1d3r@i.ua>
2021-06-04 10:08:13 +0300

    after pulling master

--- routes/api/user.js
+++ routes/api/user.js
@@ -6,14 +6,19 @@ const guard = require("../../helpers/guard");
 const upload = require("../../helpers/upload");
 
 router.post("/signup", validate.createUser, userController.reg);
+
 router.post("/login", validate.loginUser, userController.login);
+
 router.post("/logout", guard, userController.logout);
+
 router.get("/current", guard, userController.currentUser);
+
 router.patch(
   "/avatars",
   [guard, upload.single("avatar")],
   userController.updateUser
 );
+
 router.get("/verify/:verificationToken", userController.emailVerify);
 
 router.post("/verify", userController.repeatEmailVerify);
